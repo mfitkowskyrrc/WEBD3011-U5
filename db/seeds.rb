633310f6a5ed2f04ed5eb638e648a676ea34431d
mfitkowskyrrc <mfitkowsky@academic.rrc.ca>
2025-03-28 17:54:26 -0500

    added database 'products.csv' and parsing it in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "csv"
 
-require 'faker'
 Product.destroy_all
+Category.destroy_all
 
-676.times do |product|
-  Product.create!({
-    title: Faker::Commerce.unique.product_name,
-    price: Faker::Commerce.price,
-    stock_quantity: Faker::Number.between(from: 0, to: 100)
-  })
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+products.each do |product|
+  category = Category.find_or_create_by(name: category_name)
+
+  Product.create(
+    title: row["name"],
+    description: row["description"],
+    price: row["price"].to_f,
+    stock_quantity: row["stock quantity"].to_i,
+    category: category
+  )
 end
